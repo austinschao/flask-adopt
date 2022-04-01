5a982246832f641f999342fe8c28b5a6e289b465
Austin Chao <austinschao@gmail.com>
2022-04-01 12:19:20 -0700

    Completed Step 1 and Step 2

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 """Flask app for adopt app."""
 
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
@@ -21,4 +24,11 @@ db.create_all()
 #
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-toolbar = DebugToolbarExtension(app)
+
+@app.get('/')
+def display_listing():
+    """ Displays listing page for pets """
+
+    pets = Pet.query.all()
+
+    return render_template('listing_page.html', pets=pets)
--- models.py
+++ models.py
@@ -13,3 +13,24 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    """ Create a pet """
+
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+
+    name = db.Column(db.String(30), nullable = False)
+
+    species = db.Column(db.String(50), nullable = False)
+
+    photo_url = db.Column(db.Text, nullable = False, default = '')
+
+    age = db.Column(db.Text, nullable = False)
+
+    notes = db.Column(db.Text, nullable = True)
+
+    available = db.Column(db.Boolean, nullable = False, default = False)
+
